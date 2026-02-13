0d4848f943065665bc6a14904f0c133797c7b195
Rikard Grandverger <rgrandverger@vub.hr>
2026-02-13 23:30:45 +0100

    Ucenici dodani u navbar, dodan css, dodan js za delete gumb

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function potvrdiBrisanje(ime) {
+    return confirm("Jeste li sigurni da želite obrisati učenika: " + ime + "?");
+}
